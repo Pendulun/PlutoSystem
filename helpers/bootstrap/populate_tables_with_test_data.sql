65f1599e05d729e95ca8a13f50a2051f25737f83
Alexander Holmquist <48073242+Yowgf@users.noreply.github.com>
2023-04-08 20:24:32 -0300

    Add database bootstrapping (#3)

---
+++ helpers/bootstrap/populate_tables_with_test_data.sql
@@ -0,0 +1,73 @@
+---- Add users
+--
+-- Putin
+INSERT INTO users VALUES (
+  'putin',
+  'Vladimir',
+  'Putin'
+);
+-- B J
+INSERT INTO users VALUES (
+  'bj',
+  'B',
+  'J'
+);
+-- Zelensky
+INSERT INTO users VALUES (
+  'zelensky',
+  'Volodymyr',
+  'Zelensky'
+);
+
+---- Add incomes
+-- Putin
+INSERT INTO income VALUES (
+  'putin-oil',
+  'putin',
+  'Oil',
+  1000000000
+);
+INSERT INTO income VALUES (
+  'putin-extortion',
+  'putin',
+  'Extortion',
+  137000833
+);
+INSERT INTO income VALUES (
+  'putin-child-trafficking',
+  'putin',
+  'Child trafficking',
+  5000000
+);
+-- B J
+INSERT INTO income VALUES (
+  'bj-human-trafficking',
+  'bj',
+  'Human trafficking',
+  1000000000
+);
+INSERT INTO income VALUES (
+  'bj-enslaving',
+  'bj',
+  'Enslaving',
+  1000000532
+);
+INSERT INTO income VALUES (
+  'bj-covid',
+  'bj',
+  'Covid',
+  2000000000
+);
+INSERT INTO income VALUES (
+  'bj-statization',
+  'bj',
+  'Statization',
+  2147483647
+);
+-- Zelensky
+INSERT INTO income VALUES (
+  'zelensky-salary',
+  'zelensky',
+  'Presidential salary',
+  5000
+);
