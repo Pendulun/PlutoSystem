46af8d9e80f17e28e8c70b99cd19479f69ef7023
Daniel Campos <danielcampos14@hotmail.com.br>
2023-05-25 19:15:34 -0300

    tests: add expense from file

---
+++ backend/pluto/tests/_internal/adapters/test_expense_service.py
@@ -0,0 +1,38 @@
+import pytest
+import pathlib
+
+from pluto._internal.adapters.expense_service import ExpenseServiceImpl, InvalidRow
+
+from tests._internal.mocks import DatabaseMock, ConfigMock
+
+class TestExpenseService:
+
+    @pytest.fixture
+    def expense_service(self):
+        return ExpenseServiceImpl(DatabaseMock(ConfigMock.parse()))
+
+    def test_raises_invalid_num_cols(self, expense_service):
+        invalid_num_cols_file = pathlib.Path("tests/_internal/adapters/expense1.csv")
+        user_id = '1'
+        with pytest.raises(InvalidRow) as e:
+            expense_service.add_expense_from_file(invalid_num_cols_file, user_id)
+    
+    def test_raises_empty_first_col(self, expense_service):
+        invalid_num_cols_file = pathlib.Path("tests/_internal/adapters/expense2.csv")
+        user_id = '1'
+        with pytest.raises(ValueError) as e:
+            expense_service.add_expense_from_file(invalid_num_cols_file, user_id)
+    
+    def test_raises_wrong_type_for_amount(self, expense_service):
+        invalid_num_cols_file = pathlib.Path("tests/_internal/adapters/expense3.csv")
+        user_id = '1'
+        with pytest.raises(TypeError) as e:
+            expense_service.add_expense_from_file(invalid_num_cols_file, user_id)
+    
+    def test_dont_raise_for_valid_entries(self, expense_service):
+        valid_file = pathlib.Path("tests/_internal/adapters/expense4.csv")
+        user_id = '1'
+        try:
+            expense_service.add_expense_from_file(valid_file, user_id)
+        except Exception as e:
+            assert False, f"valid expense file raised an exception {e}"
