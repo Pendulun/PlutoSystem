acba517791c926b1ea3285e1cf19cf7f375bbd54
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2023-05-25 20:21:20 -0300

    Alterando algumas coisas sobre Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
-import { Home } from "./Pages"
+import React from 'react';
 
-function App() {
-  return <Home />
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Cadastro from './Pages/Cadastro/Cadastro';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// Criando c component 
+class App extends React.Component {
+    // Função que renderiza o componente
+    render() {
+        return (
+           <BrowserRouter>
+                <Routes>
+                  <Route path='/' element={<Home/>} />
+                  <Route path='/login' element={<Login/>} />
+                  <Route path='/cadastro' element={<Cadastro/>} />
+                </Routes>
+           </BrowserRouter>
+        )
+    }
+
 }
 
-export default App
+export default App;
