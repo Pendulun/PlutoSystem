0b591ba0a57895b41cbeda63647e403698c3544e
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2023-05-28 10:41:13 -0300

    Front tela perfil (#40)
    * Implementação tela de perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Cadastro from './Pages/Cadastro/Cadastro';
+import Perfil from './Pages/Perfil/Perfil';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' element={<Login/>} />
             <Route path='/home' element={<Home/>} />
             <Route path='/cadastro' element={<Cadastro/>} />
+            <Route path='/perfil' element={<Perfil/>} />
           </Routes>
       </BrowserRouter>
   )
