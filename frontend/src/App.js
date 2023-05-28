bb90bb7fe25019478445e765ae98d3c377133287
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2023-05-28 09:27:07 -0300

    salvando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import { Home } from "./Pages"
+import React from 'react';
+
+import { Home } from './Pages';
+import Perfil from './Pages/Perfil/Perfil';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  return <Home />
+  return (
+      <BrowserRouter>
+          <Routes>
+            <Route path='/perfil' element={<Perfil/>} />
+            <Route path='/home' element={<Home/>} />
+          </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App
+export default App;
