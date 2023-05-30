158f07eea93782dcb4b85861bc801e9c70973235
MrsBolinhu <ilanavirginiab@gmail.com>
2023-05-30 19:32:20 -0300

    fix[front]: adjust layout

--- frontend/src/Components/TabBar/TabBar.js
+++ frontend/src/Components/TabBar/TabBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IconHome, IconRendas, IconDespesas, IconMetas, IconEstatisticas } from './Icons';
+import { IconHome, IconRendas, IconDespesas, IconEstatisticas } from './Icons';
 import { TabBarButton } from './TabBarButton';
 
 export const TabBar = () => {
@@ -7,11 +7,10 @@ export const TabBar = () => {
   return (
     <>
       <div class="block fixed inset-x-0 bottom-0 z-10 h-[85px] bg-gray text-primaryLight drop-shadow-lg px-5 pt-5 rounded-t-lg">
-        <div id="tabs" class="flex justify-between items-center">
+        <div id="tabs" class="flex max-w-lg mx-auto justify-between items-center">
           <TabBarButton PageLink='/home' Icon={<IconHome />} Title='Início'/>
           <TabBarButton PageLink='/rendas' Icon={<IconRendas/>} Title='Rendas'/>
           <TabBarButton PageLink='/despesas' Icon={<IconDespesas />} Title='Despesas'/>
-          <TabBarButton PageLink='/metas' Icon={<IconMetas />} Title='Metas'/>
           <TabBarButton PageLink='/estatisticas' Icon={<IconEstatisticas />} Title='Estatísticas'/>
         </div>
       </div>
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -4,15 +4,21 @@ export const Home = () => {
   const { data = [] } = useQueryListUsers()
   console.log(data)
 
+  const handleClickPerfil = () => {
+    // Navegar para a tela de cadastro
+    const cadastroUrl = '/perfil';
+    window.location.href = cadastroUrl;
+  }
+
   return (
-    <>
-      <div class="flex gap-4 items-center mb-[32px]">
+    <div class="max-w-xl mx-auto">
+      <button class="flex gap-4 items-center mb-[32px]" onClick={handleClickPerfil}>
         <div class="relative w-14 h-14 overflow-hidden bg-white rounded-full dark:bg-gray-600"></div>
-        <div>
-          <h1 class="text-white font-black text-bs">Olá, Maria da Silva</h1>
-          <h2 class="text-white font-light text-sm">Bem-vinda de volta!</h2>
+        <div class="text-left">
+          <div class="text-white font-black text-bs">Olá, Maria da Silva</div>
+          <div class="text-white font-light text-sm">Bem-vind@ de volta!</div>
         </div>
-      </div>
+      </button>
       <h1 class="text-lighterGray font-black text-bs mb-[20px]">Seu balanço</h1>
       <h1 class="text-white font-semibold text-3xl mb-[20px]">R$ 15.653</h1>
       <hr class="text-lightGray mb-[20px]"/>
@@ -31,7 +37,7 @@ export const Home = () => {
           <h2 class="text-lighterGray font-medium text-bs">- R$ 34,98</h2>
         </div>
       </div>
-    </>
+    </div>
   )
 
 }
