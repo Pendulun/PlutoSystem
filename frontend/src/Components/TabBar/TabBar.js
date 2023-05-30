01dcf9e95e3bbc08eaf1dc84a2afb57c7bd1b5c1
MrsBolinhu <ilanavirginiab@gmail.com>
2023-05-30 17:09:22 -0300

    feat[front]: add tabbar

---
+++ frontend/src/Components/TabBar/TabBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IconHome, IconRendas, IconDespesas, IconMetas, IconEstatisticas } from './Icons';
+import { TabBarButton } from './TabBarButton';
+
+export const TabBar = () => {
+
+  return (
+    <>
+      <div class="block fixed inset-x-0 bottom-0 z-10 h-[85px] bg-gray text-primaryLight drop-shadow-lg px-5 pt-5 rounded-t-lg">
+        <div id="tabs" class="flex justify-between items-center">
+          <TabBarButton PageLink='/home' Icon={<IconHome />} Title='Início'/>
+          <TabBarButton PageLink='/rendas' Icon={<IconRendas/>} Title='Rendas'/>
+          <TabBarButton PageLink='/despesas' Icon={<IconDespesas />} Title='Despesas'/>
+          <TabBarButton PageLink='/metas' Icon={<IconMetas />} Title='Metas'/>
+          <TabBarButton PageLink='/estatisticas' Icon={<IconEstatisticas />} Title='Estatísticas'/>
+        </div>
+      </div>
+    </>
+	)
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { TabBar } from './Components'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const isLogged = (window.location.pathname === '/cadastro' || window.location.pathname === '/') ? false : true
+
 root.render(
   <React.StrictMode>
+    {isLogged && <TabBar/>}
     <App />
   </React.StrictMode>,
 )
