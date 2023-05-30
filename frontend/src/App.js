231d56b6d542352c4902f652bf125d8210737a8d
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2023-05-30 15:34:43 -0300

    Implementação da tela de despesas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Cadastro from './Pages/Cadastro/Cadastro';
 import Perfil from './Pages/Perfil/Perfil';
+import Despesas from './Pages/Despesas/Despesas';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
             <Route path='/home' element={<Home/>} />
             <Route path='/cadastro' element={<Cadastro/>} />
             <Route path='/perfil' element={<Perfil/>} />
+            <Route path='/despesas' element={<Despesas/>} />
           </Routes>
       </BrowserRouter>
   )
