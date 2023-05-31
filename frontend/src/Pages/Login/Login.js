3134950deb7d25626daf4241c9770bf4a48248eb
Letícia Alves <48053042+leticiasma@users.noreply.github.com>
2023-05-30 22:19:38 -0300

    Implementando a lógica de login de usuário

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,4 +1,27 @@
+import axios from 'axios';
+
 export const Login = () => {
+
+  const handleSubmit = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    const dadosLogin = {
+      email: email,
+      password: password
+    };
+  
+    axios.get('http://127.0.0.1:5000/users/', {params: dadosLogin} )
+      .then(response => {
+        // Faça algo após o login ser realizado com sucesso
+        alert('Login realizado com sucesso!');
+      })
+      .catch(error => {
+        // Faça algo em caso de erro no login
+        alert('Ocorreu um erro no login. Por favor, verifique suas credenciais e tente novamente.');
+        console.error('Ocorreu um erro:', error);
+      });
+  }
   
   const handleClickCadastro = () => {
     // Navegar para a tela de cadastro
@@ -10,9 +33,9 @@ export const Login = () => {
     <div class="flex flex-col justify-center items-center h-screen">
       <h1 class="text-white font-Inter font-bold text-2xl">Bem-vindo!</h1>
       <h2 class="text-gray3 font-Inter font-normal text-sm14 mb-[48px]">Entre no PlutoSystem!</h2>
-      <input class="w-64 h-10 rounded-t-lg outline outline-1 outline-gray4 bg-gray2 placeholder-gray4 pl-5 pr-5" type="text" placeholder="Email"></input>
-      <input class="mb-4 w-64 h-10 rounded-b-lg outline outline-1 outline-gray4 bg-gray2 placeholder-gray4 pl-5 pr-5" type="text" placeholder="Senha"></input>
-      <button class="mb-4 w-56 h-10 bg-darkTeal rounded-lg"><span class="text-white text-sm font-bold">Entrar</span></button>
+      <input id="email" class="w-64 h-10 rounded-t-lg outline outline-1 outline-gray4 bg-gray2 placeholder-gray4 pl-5 pr-5" type="text" placeholder="Email"></input>
+      <input id="password" class="mb-4 w-64 h-10 rounded-b-lg outline outline-1 outline-gray4 bg-gray2 placeholder-gray4 pl-5 pr-5" type="password" placeholder="Senha"></input>
+      <button class="mb-4 w-56 h-10 bg-darkTeal rounded-lg" onClick={handleSubmit}><span class="text-white text-sm font-bold">Entrar</span></button>
       <div class="border border-gray5 h-0 w-64"></div>
       <div class="mt-2 whitespace-no-wrap flex-col">
         <p class="text-xs flex-shrink">
