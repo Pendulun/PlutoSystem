163b9f8966a547d16e1ebdd8c17410f0747caf49
yowgf <alexthomasmol@gmail.com>
2023-06-19 18:07:24 -0300

    Run unprivileged commands in another way

--- .github/workflows/frontend-check-entrypoint.sh
+++ .github/workflows/frontend-check-entrypoint.sh
@@ -12,34 +12,4 @@ apt-get install -y postgresql-13
 $PYTHON -m pip install poetry
 chown -R postgres .
 
-su -p postgres
-pg_ctlcluster 13 main start
-psql -c "ALTER USER postgres PASSWORD 'changeme';"
-cd /root/helpers/bootstrap
-./bootstrap_local.sh
-cd ../../
-
-# Start backend
-cd backend/pluto
-$PYTHON -m poetry env use 3.10
-. .venv/bin/activate
-poetry install
-export HOST=localhost
-export PORT=5000
-export DB_USER=postgres
-export DB_PASSWORD=changeme
-export DB_HOST=localhost
-export DB_PORT=5432
-export DB_NAME=pluto
-nohup python3 pluto/main.py &
-cd ../../
-
-# Start frontend
-cd frontend
-unset PORT
-pnpm install
-nohup pnpm start &
-
-# Run cypress
-npm run env -- cypress install
-npm run env -- cypress run
+su -p postgres -s .github/workflows/frontend-check-entrypoint-postgres.sh
