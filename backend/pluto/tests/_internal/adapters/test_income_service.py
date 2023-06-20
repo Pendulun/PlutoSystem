5604e24767045af09cb8b3f5d5c43be5b1fe20aa
Daniel Campos <danielcampos14@hotmail.com.br>
2023-06-20 08:44:25 -0300

    var for test fixtures path

--- backend/pluto/tests/_internal/adapters/test_income_service.py
+++ backend/pluto/tests/_internal/adapters/test_income_service.py
@@ -6,6 +6,8 @@ from pluto._internal.utils.id import new_id
 
 from tests._internal.mocks import DatabaseMock, ConfigMock
 
+tests_fixtures = pathlib.Path(__file__).parent.parent.parent / "fixtures"
+
 class TestIncomeService:
     @pytest.fixture
     def income_service(self):
@@ -16,14 +18,14 @@ class TestIncomeService:
         return IncomeServiceImpl(real_storemgr_configured)
 
     def test_add_income_file_wrong_num_cols(self, income_service):
-        invalid_num_cols_file = pathlib.Path("tests/fixtures/income_invalid.csv")
+        invalid_num_cols_file = pathlib.Path(tests_fixtures / "income_invalid.csv")
         user_id = '1'
         with pytest.raises(ValueError) as e:
             income_service.add_income_from_file(invalid_num_cols_file, user_id)
 
     @pytest.mark.integtest
     def test_list_income(self, real_income_service):
-        oneline_file = pathlib.Path("tests/fixtures/incomes_valid.csv")
+        oneline_file = pathlib.Path(tests_fixtures / "incomes_valid.csv")
         user_id = "putin"
         real_income_service.add_income_from_file(oneline_file, user_id)
         incomes = real_income_service.list_income({})
@@ -36,7 +38,7 @@ class TestIncomeService:
             putin_user,
             bj_user,
     ):
-        oneline_file = pathlib.Path("tests/fixtures/incomes_valid.csv")
+        oneline_file = pathlib.Path(tests_fixtures / "incomes_valid.csv")
         user_id = "putin"
         real_income_service.add_income_from_file(oneline_file, user_id)
         rows = real_income_service._sm.select_star("income")
